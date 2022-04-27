c989ed0b042d0f89b3d995f93c2ce65e1eb937f9
Alexander <c19aky@cs.umu.se>
2022-04-27 13:22:25 +0200

    :-)

--- easy/26removeDuplicates.js
+++ easy/26removeDuplicates.js
@@ -2,10 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
- var removeDuplicates = function(nums) {
-    
+var removeDuplicates = function(nums) {
+    var numOfRemoved = 0;
+    for(var i = nums.length - 2; i >= 0; i--) {
+        if(nums[i] == nums[i + 1]) {
+            numOfRemoved++;
+            nums.splice(i, 1);
+        }
+    }
+    return (nums.length);
 };
 
-
-
-console.assert(removeDuplicates([1,2,2]) === (2, [1, 2]));
+console.assert(removeDuplicates([1,2,2]) === 2);
