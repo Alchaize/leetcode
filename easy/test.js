b007e1caab78928bd5c8f28ff25590412c80a5ea
Alexander <c19aky@cs.umu.se>
2022-04-27 16:11:40 +0200

    :-)

--- easy/test.js
+++ easy/test.js
@@ -1,5 +1,11 @@
 
 
+const removeElements = require('./27removeElements');
+test('Gives ([1, 2, 2], 2). Expects 1', () => {
+    expect(removeElements([1,2,2], 2)).toBe(1);
+  });
+
+
 const removeDuplicates = require('./26removeDuplicates');
 test('Gives [1, 2, 2]. Expects 2', () => {
     expect(removeDuplicates([1,2,2])).toBe(2);
